fc53db2e3ab911c5c22a143cb009f85d27c951a9
randomizerkatt <ABroderick@student.neumont.edu>
2020-08-10 12:20:54 -0600

    DbConnector Started

---
+++ DbConnector.php
@@ -0,0 +1,12 @@
+<?php
+
+//Constants
+DEFINE ('DB_USER', 'databaseUser');
+DEFINE ('DB_PSWD', 'user123');
+DEFINE ('DB_SERVER', 'localhost');
+DEFINE ('DB_NAME', 'ContentTeamDatabase');
+
+
+
+
+?>
